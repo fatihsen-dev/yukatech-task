e7d953ab69d612cfb9c167a7cfd10917d961e240
Fatih Şen <fatihydrm@outlook.com>
2022-12-09 00:05:11 +0300

    create crud system

--- store/datas.js
+++ store/datas.js
@@ -1,6 +1,6 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-const initialState = {
+let initialState = {
    datas: false,
 };
 
@@ -8,11 +8,58 @@ export const dataSlice = createSlice({
    name: "dataSlice",
    initialState,
    reducers: {
-      loadData: (state, actions) => {
-         state.datas = actions.payload;
+      loadData: (state, action) => {
+         state.datas = action.payload;
+      },
+      updateTodoStatus: (state, action) => {
+         let leftUsers = JSON.parse(localStorage.getItem("datas")).filter(
+            (db) => db.id !== action.payload.userid
+         );
+         let user = JSON.parse(localStorage.getItem("datas")).find(
+            (db) => db.id === action.payload.userid
+         );
+         let todo = { ...user.todos.find((td) => td.id === action.payload.postid) };
+         todo.status = action.payload.status;
+         let leftTodos = user.todos.filter((td) => td.id !== action.payload.postid);
+
+         console.log(leftUsers);
+         localStorage.setItem(
+            "datas",
+            JSON.stringify([...leftUsers, { ...user, todos: [...leftTodos, todo] }])
+         );
+         state.datas = JSON.parse(localStorage.getItem("datas")).find(
+            (db) => db.id === action.payload.userid
+         ).todos;
+      },
+
+      addTodo: (state, action) => {
+         state.datas = [
+            ...state.datas,
+            {
+               text: action.payload.value,
+               id: state.datas.length + 1,
+               status: "active",
+            },
+         ];
+
+         let localData = [
+            ...JSON.parse(localStorage.getItem("datas")).filter(
+               (dt) => dt.id !== action.payload.user.id
+            ),
+         ];
+         localStorage.setItem(
+            "datas",
+            JSON.stringify([
+               ...localData,
+               {
+                  ...action.payload.user,
+                  todos: [...state.datas],
+               },
+            ])
+         );
       },
    },
 });
 
-export const {} = dataSlice.actions;
+export const { loadData, updateTodoStatus, addTodo } = dataSlice.actions;
 export default dataSlice.reducer;
