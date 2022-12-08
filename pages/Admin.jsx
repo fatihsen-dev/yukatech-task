8fd8e2d24551a9b13bd271800dbd960bed5e3dcd
Fatih Şen <fatihydrm@outlook.com>
2022-12-09 00:03:35 +0300

    create admin page

---
+++ pages/Admin.jsx
@@ -0,0 +1,13 @@
+import Head from "next/head";
+
+export default function Admin() {
+   return (
+      <div className='bg-dark h-full'>
+         <Head>
+            <title>Admin</title>
+            <meta name='description' content='Development by Fatih Şen' />
+            <link rel='icon' href='/favicon.ico' />
+         </Head>
+      </div>
+   );
+}
